656621cf419458b940844bee835d28288b148d2f
Jenna <jenna.nicospace@gmail.com>
2025-04-27 18:28:24 -0400

    Fixed bookmark page

--- src/Bookmarks.js
+++ src/Bookmarks.js
@@ -35,8 +35,8 @@ function Bookmarks(props) {
     return (
         <div className="Resources">
             <Header name={props.headerText} />
-            <div className = "cardContainer">
             <div class="splitResources">
+            <div className = "cardContainer">
                 {ids.map((gid, index) => (
                     <>
                         {props.data
@@ -45,31 +45,43 @@ function Bookmarks(props) {
                                 <>
                                     {
                                         Object.hasOwn(filteredItem, 'links') || Object.hasOwn(filteredItem, 'logo') ?
-                                            <div className="oneProgram">
-
-
-                                                {Object.hasOwn(filteredItem, 'links') ?
-                                                    <span className="links">
-                                                        {filteredItem.links.map((link) => <a href={link.link}><button target="_blank">{link.text}</button></a>)}
-                                                    </span> : null}
-                                                {Object.hasOwn(filteredItem, 'description') && filteredItem.description != "" ?
-                                                    <span className="text">
-                                                        {filteredItem.description.map((desc) => (<p>{desc}</p>))}
-                                                    </span> : null}
-
-                                                {Object.hasOwn(filteredItem, 'logo') ?
-                                                    <span className="logo"> <img src={filteredItem.logo} alt={filteredItem.sectionTitle} /> </span>
-                                                    : null}
-                                                <span className="bookmarks"><button onClick={() => UpdateStorage(filteredItem.gid)}><img src="../imgs/bookmark.png"/></button></span>
-
-                                            </div>
-                                            :
-                                            <div className="onlyText">
-                                                {Object.hasOwn(filteredItem, 'description') && filteredItem.description != "" ?
-                                                    <span className="text">
-                                                        {filteredItem.description.map((desc) => (<p>{desc}</p>))}
-                                                    </span> : null}
-                                            </div>
+                                                
+                                                
+                                        <div className="oneProgram">
+                                            
+                                            {/* Commented out links, will move to router page */}
+                                            {/* {Object.hasOwn(filteredItem, 'links') ?
+                                                <span className="links">
+                                                    {filteredItem.links.map((link) => <a href={link.link}><button target="_blank">{link.text}</button></a>)}
+                                                </span> : null} */}
+                                            {Object.hasOwn(filteredItem, 'description') && filteredItem.description != "" ?
+                                                <span className="text">
+                                                    {filteredItem.description.map((desc) => (<p>{desc}</p>))}
+                                                </span> : null}
+                                            <div className="logoLinks">  
+                                            {Object.hasOwn(filteredItem, 'logo') ?
+                                                <span className="logo"> <img src={filteredItem.logo} alt={filteredItem.sectionTitle} /> </span>
+                                                : null}
+                                            <span className="links">
+                                            {Object.hasOwn(filteredItem, 'links') ?
+                                                <span className="siteLink">
+                                                    {<a href={filteredItem.links[0].link}><button target="_blank"><img className = "smallButtonSVG" src = '../imgs/newPageButton.svg'></img></button></a>}
+                                                </span> : null}
+                                                
+                                            <span className="shareLink">
+                                                <a href={"#/page/" + filteredItem.gid}><button target="_blank">{<img className = "smallButtonSVG" src = '../imgs/infoButton.svg'></img>}</button></a>
+                                            </span>
+                                            <span className="bookmarks"><button onClick={() => UpdateStorage(filteredItem.gid)}>{<img className = "smallButtonSVG" src = '../imgs/bookmarkButton.svg'></img>}</button></span>
+                                            </span>
+                                            </div> 
+                                        </div>
+                                        :
+                                        <div className="onlyText">
+                                            {Object.hasOwn(filteredItem, 'description') && filteredItem.description != "" ?
+                                                <span className="text">
+                                                    {filteredItem.description.map((desc) => (<p>{desc}</p>))}
+                                                </span> : null}
+                                        </div>
                                     }
                                 </>
                             ))}
