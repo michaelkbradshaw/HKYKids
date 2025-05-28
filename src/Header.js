44914d0f09f3a2e97bd0471d0fec93a09bdb6a3a
Michael Bradshaw <michael.bradshaw@centre.edu>
2025-05-28 17:25:34 -0400

    Fixed menu bug when using textFit library

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,10 @@
 import React, {Component} from 'react'; 
 import Menu from './Menu.js';
 import './Header.css';
-//import { textFit } from 'textfit';
-//import 'textfit.js';
-//import 'textfit';
+
 const textFit = require("textfit");
 
-/*function Header(props){
-    return(
-    <div className="Header">
-        <div className="menu">{<Menu />}</div>
-        <a href="/" className="logoLink">
-            <img src="../imgs/HKYKIDSLOGO.png" className='logoImg'/>
-        </a>
-        <h1 class="fitMe">{props.name}</h1>
-    </div>
-    )
-}
-*/
+
 
 class Header extends Component
 {
@@ -25,7 +12,7 @@ class Header extends Component
     redrawText()
     {
         console.log("updating");
-        textFit(document.getElementsByClassName('Header'), {alignHoriz: true, alignVert: true});
+        textFit(document.getElementsByClassName('fitMe'), {alignHoriz: true, alignVert: true});
     }
 
     componentDidMount()
@@ -42,7 +29,6 @@ class Header extends Component
 
     componentDidUpdate()
     {
-        //console.log("TF",textFit);
         console.log("updating");
         this.redrawText();
     }
@@ -54,7 +40,9 @@ class Header extends Component
             <a href="/" className="logoLink">
                 <img src="../imgs/HKYKIDSLOGO.png" alt="walking family" className='logoImg'/>
             </a>
-            <h1 className="fitMe">{this.props.name}</h1>
+            <div className="fitMe">
+                <h1 >{this.props.name}</h1>
+            </div>
         </div>
 
         )
