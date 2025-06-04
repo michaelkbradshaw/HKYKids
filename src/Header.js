63c7458baa81a07b1ce63c5a49e03ba6a7c44624
Michael Bradshaw <michael.bradshaw@centre.edu>
2025-06-04 17:49:08 -0400

    refactored header to functional component as extra complexity not needed

--- src/Header.js
+++ src/Header.js
@@ -1,66 +1,9 @@
-import React, {Component} from 'react'; 
+import React from 'react'; 
 import Menu from './Menu.js';
 import './Header.css';
 
-//const textFit = require("textfit");
-//import { AutoTextSize } from 'auto-text-size'
-
-
-class Header extends Component
+function Header(props)
 {
-    constructor(props)
-    {   super(props);
-        this.state = {title:props.name};
-    }
-
-    static getDerivedStateFromProps(props,state)
-    {
-        console.log("resetting state?",props,state);
-        return {title:props.name};
-    }
-
-
-    redrawText()
-    {
-        console.log("redrawing");
-        //textFit(document.getElementsByClassName('fitMe'), {alignHoriz: true, alignVert: true});
-        //this.forceUpdate()
-    }
-
-    componentDidMount()
-    {
-        console.log("mount");
-        this.redrawText()
-        window.addEventListener('resize', this.redrawText);
-    }
-
-    componentWillUnmount()
-    {
-        window.removeEventListener('resize', this.redrawText);
-    }
-
-    /*
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log("afterRender before update",this.props.name);
-        this.redrawText();
-      }
-*/
-    shouldComponentUpdate()
-    {
-        console.log("should update");
-        return true;
-    }
-
-    componentDidUpdate()
-    {
-        console.log("Did updating",this.props.name);
-        this.redrawText();
-    }
-
-
-
-    render() {
-        console.log("rendering header",this.props.name,this.state.title);
         return (
         <div className="Header">
             <div className="menu">{<Menu />}</div>
@@ -69,14 +12,12 @@ class Header extends Component
             </a>
             
             <div className="vertCenter">
-                <h1 >{this.state.title}</h1>
+                <h1 >{props.headerText}</h1>
             </div>
           
         </div>
 
         )
-    }
-
 }
 
 export default Header;
